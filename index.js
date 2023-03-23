51d2cb5e34f61c85d2891fe18d1c3f97781d17ca
nathan <nathanmageka6@gmail.com>
2023-03-23 13:13:26 +0300

    nathan

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(name, birthday) {
+    const myArray = [];
+    for (let i = 0; i<name.length; i++){
+        const text =
+        `Thank you, (name[1]), for the wonderful ${birthday} gift!` ;
+        myArray.push(text);
+    }
+    return myArray;
+}
+function countDown(number) {
+    do {
+        console.log(number);
+        number--;
+    }
+    while (number >= 0);
+}
